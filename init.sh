3e6ec054ee7f03b53fe1c2da65b93fe7658a0004
CielNeko <cielneko@proton.me>
2024-01-13 10:35:38 +0800

    chore: change sone config
    Signed-off-by: CielNeko <cielneko@proton.me>

--- init.sh
+++
@@ -1,6 +0,0 @@
-sudo apt update && sudo apt upgrade -y
-sudo apt install -y zsh git curl wget
-sudo useradd kusuri -m -s /bin/zsh
-sudo passwd kusuri -d
-sudo usermod -aG sudo kusuri
-sudo -u kusuri 'mkdir -p $HOME/.ssh && echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOw9UmCECIUlvD4plF9y+Zp3725lXQI9bQLlrKkkvMIW" >> $HOME/.ssh/authorized_keys && echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEcGRSPyZ2ie5sJcXstxbTua8N+6pXlw3Rh+l8j1/bDf" >> $HOME/.ssh/authorized_keys'
